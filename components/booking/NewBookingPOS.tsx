ca5751093b97859ba34bf4f92b81256c02082e6f
dirhaam <dirhamrozi@gmail.com>
2025-11-18 11:18:54 +0700

    Add DialogDescription import to NewBookingPOS - Fix build error
    DialogDescription was used but not imported, causing TypeScript compile error.

--- components/booking/NewBookingPOS.tsx
+++ components/booking/NewBookingPOS.tsx
@@ -5,6 +5,7 @@ import {
   Dialog,
   DialogContent,
   DialogTitle,
+  DialogDescription,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
